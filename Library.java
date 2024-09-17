c0c3fbc4d24a5c3a94b373caf9fa858b381ae02b
TonyMontoyaO <amontoya.olivas15@gmail.com>
2024-09-17 16:27:24 -0600

    Fixed Inventory Class, Library Class can now add, remove, and print

--- Library.java
+++ Library.java
@@ -3,13 +3,15 @@ public class Library {
     public Library(){
         recipeList = new List<>();
     }
+    //add Recipe object to Library List
     public void addRecipe(Recipe recipe){
         recipeList.add(recipe);
     }
+    //removes Recipe object from Library List
     public void removeRecipe(Recipe recipe){
         recipeList.remove(recipe);
     }
-
+    //get Recipe from string name parameter
     public Recipe getRecipe(String name) {
         for (int i = 0; i < recipeList.size; i++) {
             if (recipeList.get(i).name.equals(name)) {
@@ -18,7 +20,7 @@ public class Library {
         }
         return null;
     }
-
+    //prints Library list
     public void printRecipeList(){
         for (int i = 0; i < recipeList.size; i++) {
             recipeList.get(i).printRecipe();
