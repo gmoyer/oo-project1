129082ca9234c11f93be653cde2aa055362154d7
Julie Jarzemsky <jjarzem@gmail.com>
2024-09-20 10:18:44 -0600

    error comments, constants

--- Brewery.java
+++ Brewery.java
@@ -1,4 +1,14 @@
 public class Brewery {
+    // could move ingredient names and recipes to two enums or move these to
+    // a different class to improve Brewery cohesion
+    public static final String APPLE = "apple";
+    public static final String FLOUR = "flour";
+    public static final String SUGAR = "sugar";
+    public static final String WHEAT = "wheat";
+    public static final String WATER = "water";
+    public static final String YEAST = "yeast";
+    public static final String APPLE_PIE = "apple pie";
+    public static final String BEER = "beer";
 
     Inventory inventory;
     ContainerManager containerManager;
@@ -15,26 +25,26 @@ public class Brewery {
         Brewery brewery = new Brewery();
 
         // Add ingredients to inventory
-        brewery.inventory.addIngredient(new Ingredient("apple", 100));
-        brewery.inventory.addIngredient(new Ingredient("flour", 100));
-        brewery.inventory.addIngredient(new Ingredient("sugar", 100));
-        brewery.inventory.addIngredient(new Ingredient("wheat", 100));
-        brewery.inventory.addIngredient(new Ingredient("water", 100));
-        brewery.inventory.addIngredient(new Ingredient("yeast", 100));
+        brewery.inventory.addIngredient(new Ingredient(APPLE, 100));
+        brewery.inventory.addIngredient(new Ingredient(FLOUR, 100));
+        brewery.inventory.addIngredient(new Ingredient(SUGAR, 100));
+        brewery.inventory.addIngredient(new Ingredient(WHEAT, 100));
+        brewery.inventory.addIngredient(new Ingredient(WATER, 100));
+        brewery.inventory.addIngredient(new Ingredient(YEAST, 100));
 
         brewery.inventory.printInventory();
 
         // Create recipes
-        Recipe recipe1 = new Recipe("apple pie", 5);
-        recipe1.addIngredient(new Ingredient("apple", 3));
-        recipe1.addIngredient(new Ingredient("flour", 2));
-        recipe1.addIngredient(new Ingredient("sugar", 1));
+        Recipe recipe1 = new Recipe(APPLE_PIE, 5);
+        recipe1.addIngredient(new Ingredient(APPLE, 3));
+        recipe1.addIngredient(new Ingredient(FLOUR, 2));
+        recipe1.addIngredient(new Ingredient(SUGAR, 1));
         recipe1.printRecipe();
 
-        Recipe recipe2 = new Recipe("beer", 20);
-        recipe2.addIngredient(new Ingredient("wheat", 5));
-        recipe2.addIngredient(new Ingredient("water", 10));
-        recipe2.addIngredient(new Ingredient("yeast", 1));
+        Recipe recipe2 = new Recipe(BEER, 20);
+        recipe2.addIngredient(new Ingredient(WHEAT, 5));
+        recipe2.addIngredient(new Ingredient(WATER, 10));
+        recipe2.addIngredient(new Ingredient(YEAST, 1));
         recipe2.printRecipe();
 
         // Add recipes to library
@@ -42,12 +52,21 @@ public class Brewery {
         brewery.library.addRecipe(recipe2);
 
         // Create batches
-        brewery.createBatch(brewery.library.getRecipe("apple pie"), 10);
-        brewery.createBatch(brewery.library.getRecipe("beer"), 20);
-        brewery.createBatch(brewery.library.getRecipe("apple pie"), 200); // This requires a restock
-        brewery.createBatch(brewery.library.getRecipe("beer"), 10);
-        brewery.createBatch(brewery.library.getRecipe("beer"), 40);
-        brewery.createBatch(brewery.library.getRecipe("beer"), 20); // This requires batch cleaning
+        brewery.createBatch(brewery.library.getRecipe(APPLE_PIE), 10);
+        brewery.createBatch(brewery.library.getRecipe(BEER), 20);
+        brewery.createBatch(brewery.library.getRecipe(APPLE_PIE), 200); // This requires a restock
+        brewery.createBatch(brewery.library.getRecipe(BEER), 10);
+        brewery.createBatch(brewery.library.getRecipe(BEER), 40);
+        brewery.createBatch(brewery.library.getRecipe(BEER), 20); // This requires batch cleaning
+
+        // testing error handling
+//        brewery.createBatch(brewery.library.getRecipe("nonexistent"), 10); // results in NPE
+        // test 0 servings
+        brewery.createBatch(brewery.library.getRecipe(APPLE_PIE), 0); // works fine
+        // test large number of servings
+        // no container cleanings seems suspicious
+        // I think you're missing a check on if there are enough containers for total servings
+        brewery.createBatch(brewery.library.getRecipe(APPLE_PIE), 10000);
 
         // Print inventory
         brewery.inventory.printInventory();
